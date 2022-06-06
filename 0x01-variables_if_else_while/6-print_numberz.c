08f7e3af2840cd4e77c35adb1248a9b25b71173d
Dominion <adinoyibaidoo@gmail.com>
2022-06-06 18:26:37 +0000

    printing numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point for my code
+ *
+ * Description: printing numbers
+ * Return: always return 0
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 9; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}

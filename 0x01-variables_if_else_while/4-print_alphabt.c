c39a02c6a8b7f1b344f58149ecdbdec8f6ae948b
Dominion <adinoyibaidoo@gmail.com>
2022-06-06 21:02:26 +0000

    more alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point to the code
+ *
+ * Description: Prints the alphabets except e and q
+ * Return: returns 0
+ */
+int main(void)
+{
+int a;
+for (a = 'a'; a <= 'z'; a++)
+{
+if (a == 'e' || a == 'q')
+{
+a++;
+}
+}
+putchar ('\n');
+return (0);
+}

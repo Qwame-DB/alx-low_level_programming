6ec2e008f02226ccb8c76b25adc1749a6806b2de
Dominion <adinoyibaidoo@gmail.com>
2022-06-06 17:58:44 +0000

    printing base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point to all of my code
+ *
+ * Description: prints all base 10 numbers
+ * Return: always returns 0
+ */
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+return (0);
+}

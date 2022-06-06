12ddd94b6f893b158074733180d76754e717608d
Dominion <adinoyibaidoo@gmail.com>
2022-06-06 20:01:23 +0000

    Base 16 numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point to the code
+ *
+ * Description: Base 16 numbers
+ * Return: returns 0
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 15; a++)
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}

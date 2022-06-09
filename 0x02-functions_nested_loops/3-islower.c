8320e2da00899041f16482b79d3fde1f42ff990a
Dominion <adinoyibaidoo@gmail.com>
2022-06-09 16:26:59 +0000

    lowercase characters

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - check the code
+ *
+ * Description: checks for lowercase letters
+ * Return: return 1 if c is lowercase
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

56041748f9f0b7b76eff6c89d5bf349527abd4ff
Dominion <adinoyibaidoo@gmail.com>
2022-06-09 17:34:52 +0000

    print the sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_sign - check the code
+ * @n: a variable
+ *
+ * Description: printing the sign of a number
+ * Return: return 0, 1, or -1
+ */
+int print_sign(int n)
+{
+if (n > '0')
+{
+return (1) && _putchar ('+');
+}
+else if (n = '0')
+{
+return (0) && _putchar ('0');
+}
+else if (n < '0')
+{
+return (-1) && _putchar ('-');
+}
+}

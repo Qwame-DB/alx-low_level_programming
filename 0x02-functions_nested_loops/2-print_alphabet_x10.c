5baf3e34562629aea7512250c0297e4e94489bc5
Dominion <adinoyibaidoo@gmail.com>
2022-06-09 12:45:58 +0000

    print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Description: print the alphabets x10
+ * Return: void type doesn't return any value
+ */
+void print_alphabet_x10(void)
+{
+int a;
+int b;
+for (a = '0'; a <= '9'; a++)
+{
+for (b = 'a'; b <= 'z'; b++)
+{
+_putchar ('b');
+_putchar ('\n');
+}
+}
+}

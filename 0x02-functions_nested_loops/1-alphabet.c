4a2c2ed5fc20f4c65f813a003a1420a77d53dc66
Dominion <adinoyibaidoo@gmail.com>
2022-06-09 12:22:04 +0000

    lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Description: prints all alphabets in lowercase
+ * Return: void doesn't return a value
+ */
+void print_alphabet(void)
+{
+int i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar (i);
+}
+}

f705cefb9f8a77a00b4115fa95f7911cb9827f0d
Dominion <adinoyibaidoo@gmail.com>
2022-06-09 19:15:54 +0000

    printing the signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,17 +9,17 @@
  */
 int print_sign(int n)
 {
-if (n > '0')
+if (n > 30)
 {
 return (1);
 _putchar ('+');
 }
-else if (n == '0')
+else if (n == 30)
 {
 return (0);
 _putchar ('0');
 }
-else if (n < '0')
+else if (n < 30)
 {
 return (-1);
 _putchar ('-');

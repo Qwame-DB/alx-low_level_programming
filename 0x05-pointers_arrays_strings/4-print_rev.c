13589870ecf8c6559c5ce52a692137330994fd7c
Dominion <adinoyibaidoo@gmail.com>
2022-06-15 20:41:18 +0000

    the reverse of a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - check the code
+ *
+ * @s: pointer to a string
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+int count;
+int j = 0;
+for (count = 0; *(s + count) != '\0'; count++)
+{
+j++;
+}
+return (j);
+}
+
+/**
+ * print_rev - print reverse of a string
+ *
+ * @s: pointer to a variable
+ * Return: void
+ */
+void print_rev(char *s)
+{
+int len, i;
+
+len = _strlen(s);
+for (i = len -1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
+

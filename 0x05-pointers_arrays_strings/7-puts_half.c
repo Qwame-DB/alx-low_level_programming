e4571083e909c1a5b2315a14d3f46c39549c7a95
Dominion <adinoyibaidoo@gmail.com>
2022-06-16 20:16:54 +0000

    print the last half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - prints the length of a string
+ *
+ * @s: pointer to a string
+ * Return: Always 0
+ */
+int _strlen(char *s)
+{
+int count;
+int j = 0;
+for (count = 0; *(s + count) != '\0'; count++)
+{
+j++;
+}
+return (j);
+}
+
+/**
+ * puts_half - prints half of the characters
+ *
+ * @str: pointer to a string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int loop, len;
+
+len = _strlen(str);
+for (loop = len / 2; loop <=  len; loop++)
+{
+putchar(str[loop]);
+}
+putchar('\n');
+}

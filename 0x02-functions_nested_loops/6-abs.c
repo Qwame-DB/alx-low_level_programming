34c6afe7c87eddecd90efbbcd787a0e1e15bbced
Dominion <adinoyibaidoo@gmail.com>
2022-06-17 19:25:12 +0000

    prints the integer value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
+ * _abs - prints the absolute integer
  *
- *
- *
- *
- *
+ * @int: type of a variable
+ * Return: always 0
  */
 int _abs(int)
 {
-_putchar(
+int ru;
+
+ru = srlen(int);
+if (ru > 1)
+{
+_putchar(srlen(int[1]));
+}
+return (0);
 }

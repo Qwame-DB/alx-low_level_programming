07c73cbb0293b38e93b8a8a98c9be3ddbe77c7e3
Dominion <adinoyibaidoo@gmail.com>
2022-06-17 18:13:54 +0000

    print to stderr'

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - print a string
+ *
+ * Return: return one
+ */
+int main(void)
+{
+char *y = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+fputs(y, stderr);
+return (1);
+}
+
